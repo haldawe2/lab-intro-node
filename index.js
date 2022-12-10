942cca38c8d559d3371070ccdc341e5a58cd6ef0
Gerard Guasch <haldawe@hotmail.com>
2022-12-10 12:34:56 +0100

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {
+      this.items.push(item);
+      this.length = this.items.length;
+      return;
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (item < this.items[i]) {
+        if (i === 0) {
+          this.items.unshift(item);
+          break;
+        } else {
+          this.items.splice(i, 0, item);
+          break;
+        }
+      } else {
+        this.items.push(item);
+        break
+      }
+    }
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length - 1) {
+      throw new Error ("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
